330a8d17a5dea1c8a1912edb5f28e8e843425096
mrakib007 <mrakibhasan199744@gmail.com>
2023-12-11 11:24:41 +0600

    Added some changes

---
+++ src/app/config/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = require("dotenv");
+var path_1 = require("path");
+dotenv_1.default.config({ path: path_1.default.join((process.cwd(), '.env')) });
+exports.default = {
+    port: process.env.PORT,
+};
---
+++ src/app/config/modules/user/user.validation.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+const getAllUsers = async (req:Request,res:Response) =>{
+    try{
+    }
+}
